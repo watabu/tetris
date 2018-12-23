5a9ac34397d650489e820d5295a07b50938461c1
watabu <4618085@ed.tus.ac.jp>
2018-12-23 23:54:14 +0900

    Title and etc from watabu

---
+++ watabu/test2D/Assets/Scripts/ConConect2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // UIコンポーネントの使用
+
+
+public class ConConect2 : MonoBehaviour
+{
+    Button back;
+    Button reconect1;
+    Button reconect2;
+    Button enter;
+    // Use this for initialization
+    void Start()
+    {
+        back = GameObject.Find("/Canvas/Button back").GetComponent<Button>();
+        reconect1 = GameObject.Find("/Canvas/Button reconect").GetComponent<Button>();
+        reconect2 = GameObject.Find("/Canvas/Button reconect (1)").GetComponent<Button>();
+        enter = GameObject.Find("/Canvas/Button enter").GetComponent<Button>();
+        reconect1.Select();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ watabu/test2D/Assets/Scripts/Input.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using GamepadInput;
+
+public class PlayerCon : MonoBehaviour
+{
+    public enum KEY_INPUT
+    {
+        KEY_HORIZON,//右が１、左がー１
+        KEY_VERTICAL,//上が１、下がー１
+        KEY_SUBMIT,//Aボタン
+        KEY_CANCEL,//Bボタン
+        KEY_HOLD//Lボタン or R
+    }
+
+    public int GetInput(int playerNum, KEY_INPUT key)//
+    {
+        GamepadState state = GamePad.GetState((GamePad.Index)playerNum);//入力状態
+        switch (key)
+        {
+            case KEY_INPUT.KEY_HORIZON:
+                if (state.LeftTrigger > 0) return 1;//右
+                if(state.LeftTrigger <0)return -1;//左
+                break;
+            case KEY_INPUT.KEY_VERTICAL:
+                if (state.RightTrigger > 0) return 1;
+                if (state.RightTrigger < 0) return -1;
+                break;
+            case KEY_INPUT.KEY_SUBMIT:
+                if (state.B) return 1;
+                break;
+
+            case KEY_INPUT.KEY_CANCEL:
+                if (state.A) return 1;
+                break;
+            case KEY_INPUT.KEY_HOLD:
+                if (state.LeftShoulder) return 1;
+                if (state.RightShoulder) return 1;
+                break;
+        }
+
+        return 0;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
