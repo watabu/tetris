ff55fa3793f729d1d9c9185b049f64649ae47dfc
watabu <4618085@ed.tus.ac.jp>
2019-01-07 22:57:40 +0900

    from watabu

---
+++ watabu/test2D/Assets/Scripts/SceneMgrs/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//もっぱらデバッグ用
+public class Game : MonoBehaviour {
+    PlayerInput PlInput = GetComponent<PlayerInput>(); //できない！！！！！！なんで！！！！
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Debug start\n");
+        PlInput.ChangePlConkind(0, PlayerInfo.eConKind.JOYCON);
+	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (PlInput.GetInput(0, PlayerInput.KEY_INPUT.KEY_SUBMIT) == 1)
+        {
+            Debug.Log("0 Submit");
+
+        }
+    }
+}
