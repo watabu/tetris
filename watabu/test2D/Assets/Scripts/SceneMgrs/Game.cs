070c8a40a77b42de72801c2fc45d767518f16258
watabu <4618085@ed.tus.ac.jp>
2019-01-30 23:50:59 +0900

    sound and visual update

--- watabu/test2D/Assets/Scripts/SceneMgrs/Game.cs
+++ watabu/test2D/Assets/Scripts/SceneMgrs/Game.cs
@@ -1,24 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(PlayerInput))]
+//もっぱらデバッグ用
 public class Game : MonoBehaviour {
-    PlayerInput PlInput;
+   
+    public PlInput PI; 
 	// Use this for initialization
 	void Start () {
-        PlInput = GetComponent<PlayerInput>();
-        Debug.Log("Debug start\n");
-        PlInput.ChangePlConkind(0, PlayerInfo.eConKind.JOYCON);
-	}
+        PI = GetComponent<PlInput>();
+        Debug.Log("Game's Start");
+        PI.ChangePlConkind(0, PlInput.ConKind.KEYBOARD1);
+        PI.ChangePlConkind(1, PlInput.ConKind.KEYBOARD2);
+        Debug.Log(PlInput.Player[0].ConKind);
+        Debug.Log(PlInput.Player[1].ConKind);
+    }
     // Update is called once per frame
     void Update()
     {
 
-        if (PlInput.GetInput(0, PlayerInput.KEY_INPUT.KEY_SUBMIT) == 1)
+        if (PI.GetInput(0, PlInput.Key.KEY_SUBMIT) == 1)
         {
             Debug.Log("0 Submit");
 
+        }
+        if (PI.GetInput(1, PlInput.Key.KEY_SUBMIT) == 1)
+        {
+            Debug.Log("1 Submit");
         }
     }
 }
--- watabu/test2D/Assets/Scripts/SceneMgrs/Title.cs
+++ watabu/test2D/Assets/Scripts/SceneMgrs/Title.cs
@@ -9,22 +9,24 @@ public class Title : MonoBehaviour
 
     Button Player1;
     Button Player2;
+    public AudioClip audioClip;
+    AudioSource audioSource;
 
-       
-  
-    
+
     // Use this for initialization
     void Start()
     {
         Player1 = GameObject.Find("/Canvas/1 Player").GetComponent<Button>();
         Player2 = GameObject.Find("/Canvas/2 Player").GetComponent<Button>();
         Player1.Select();
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
     }
 
     // Update is called once per frame
     void Update()
     {
-   
+        audioSource.Play();
     }
 }
 
